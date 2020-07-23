c4505865d24b3a26ee0377aa6d9331f37e4ce32b
PIRANHA <kobeel@163.com>
2020-07-23 20:41:33 +0800

    update

--- ts-tic-tac-toe/dist/index.js
+++ ts-tic-tac-toe/dist/index.js
@@ -1,5 +1,15 @@
 "use strict";
+// 定义当前玩家
+var Player;
+(function (Player) {
+    Player["X"] = "x";
+    Player["O"] = "o";
+})(Player || (Player = {}));
+var currentPlayer = Player.X;
 var cells = document.querySelectorAll('.cell');
+console.dir(cells[0]);
+// 获取游戏面板
+var gameBoard = document.querySelector('#board');
 cells.forEach(function (item) {
     var cell = item;
     cell.addEventListener('click', handleCellClick, { once: true });
@@ -7,8 +17,29 @@ cells.forEach(function (item) {
 // 棋盘中单元格的callback函数
 function handleCellClick(e) {
     var target = e.target;
-    target.classList.add('x');
-    // console.log(e.target)
+    target.classList.add(currentPlayer);
+    // 调用判赢函数
+    var isWin = judgeWin(currentPlayer);
+    if (isWin) {
+        console.log(currentPlayer + "\u83B7\u80DC\u4E86");
+    }
+    // 切换玩家
+    currentPlayer = currentPlayer === Player.X ? Player.O : Player.X;
+    gameBoard.classList.remove(Player.X, Player.O);
+    gameBoard.classList.add(currentPlayer);
 }
-// 切换玩家
-var currentPlayer;
+// 获胜的8种情况
+var winsArr = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+// 判断是否获胜
+function judgeWin(player) {
+    return true;
+}
--- ts-tic-tac-toe/index.ts
+++ ts-tic-tac-toe/index.ts
@@ -1,15 +1,51 @@
-let cells = document.querySelectorAll('.cell')
+// 定义当前玩家
+enum Player {
+  X = 'x',
+  O = 'o',
+}
+let currentPlayer: Player = Player.X
+
+let cells = document.querySelectorAll('.cell') as NodeList
+console.dir(cells[0])
+
+// 获取游戏面板
+let gameBoard = document.querySelector('#board') as HTMLDivElement
+
 cells.forEach((item) => {
   let cell = item as HTMLDivElement
   cell.addEventListener('click', handleCellClick, { once: true })
 })
 
 // 棋盘中单元格的callback函数
-function handleCellClick(e: any): void {
+function handleCellClick(e: MouseEvent): void {
   let target = e.target as HTMLDivElement
-  target.classList.add('x')
-  // console.log(e.target)
+  target.classList.add(currentPlayer)
+
+  // 调用判赢函数
+  let isWin = judgeWin(currentPlayer)
+  if (isWin) {
+    console.log(`${currentPlayer}获胜了`)
+  }
+
+  // 切换玩家
+  currentPlayer = currentPlayer === Player.X ? Player.O : Player.X
+  gameBoard.classList.remove(Player.X, Player.O)
+  gameBoard.classList.add(currentPlayer)
 }
 
-// 切换玩家
-let currentPlayer
+// 获胜的8种情况
+const winsArr: number[][] = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
+// 判断是否获胜
+function judgeWin(player: Player): boolean {
+  return true
+}
